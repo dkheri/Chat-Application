67527db388fb22d45c25ef0b28b64bebef9b7774
MelulekiDube <mellarmellah@gmail.com>
2018-03-14 18:32:17 +0200

    Before merging Messages

--- src/Message.java
+++ src/Message.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -19,8 +18,7 @@ public class Message implements Serializable{
     public final String message;
     
     
-//    private File filetoSend;
-    
+//    private File filetoSend
     public Message(String mType, String recipent, String destFor, String message) {
         this.mType = mType;
         this.recipent = recipent;
@@ -30,45 +28,3 @@ public class Message implements Serializable{
 
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-import java.io.Serializable;
-
-/**
- *
- * @author Meluleki
- */
-public class Message implements Serializable{
-    public final String mType;
-    public final String recipent;
-    public final String destFor;
-    public final String message;
-    
-    
-//    private File filetoSend;
-
-    public Message(String mType, String recipent, String sender, String message) {
-        this.mType = mType;
-        this.recipent = recipent;
-        this.destFor = destFor;
-        this.message = message;
-    }
-
-    public Message(String mType,String message)
-    {
-        this.mType = mType;
-        this.recipent = null;
-        this.destFor = null
-        this.message = message;
-
-    }
-
-    
-}
->>>>>>> de880564ea2d66c713dbd7b12e30d171ded7484b
