bc5eda43ecb09cbcb0432e5511d5fbbb4a2a3f5b
MelulekiDube <mellarmellah@gmail.com>
2018-03-14 18:30:31 +0200

    Before getting Message

--- src/CServer.java
+++ src/CServer.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -45,3 +46,48 @@ public class CServer {
     
     
 }
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.io.IOException;
+
+/**
+ *
+ * @author Meluleki
+ */
+public class CServer {
+
+    private static long Port;
+    private final ServerSocket server;
+    private static ArrayList<Socket> clientSockets;
+    private static ArrayList<String> loginNames;
+
+    public CServer() throws IOException {
+        server = new ServerSocket((int) Values.SERVER_PORT_NUMBER);
+        clientSockets = new ArrayList<>();
+        loginNames = new ArrayList<>();
+        connect();
+    }
+
+    public void connect() throws IOException {
+        while (true) {
+            Socket clienSocket = this.server.accept();
+            Thread runThread = new Thread(new AcceptClient(clienSocket));
+        }
+    }
+
+    public static void addClient() {
+
+    }
+
+    public static void removeClient() {
+
+    }
+}
+>>>>>>> de880564ea2d66c713dbd7b12e30d171ded7484b
