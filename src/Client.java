ba8e800a8a46b5cb5c90a50cef55ffec9a613041
David Kheri <david.kheri@yahoo.com>
2018-03-15 09:33:39 +0200

    UpdateTextArea Method Called takes in one Argument that is the Display Message

--- src/Client.java
+++ src/Client.java
@@ -41,7 +41,8 @@ public class Client implements Runnable{
             String textMessage=msg.message;
             String sender=msg.sender;
             String recipent=msg.recipent;
-            UpdateTextArea(textMessage,sender,recipent);
+            String displayMessage="["+sender+" to "+recipent+"]:"+msg;
+            UpdateTextArea(displayMessage);
         }
 
     }
@@ -78,9 +79,9 @@ public class Client implements Runnable{
 
 
 
-    public static void UpdateTextArea(String msg,String sender,String recipent) throws IOException {
+    public static void UpdateTextArea(String displayMessage) throws IOException {
         
-        String displayMessage="["+sender+" to "+recipent+"]:"+msg;
+        
         cg.putToTA(displayMessage);
         
 
@@ -119,7 +120,7 @@ public class Client implements Runnable{
             // Client.cg.populateListView(r);
             // System.out.println(Arrays.toString(r.toArray()));
 
-            
+
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
